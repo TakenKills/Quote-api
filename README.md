9b93500a88a4365b4d9e60cbcb85eea16d94982a
TakenKills <dragonagario2@gmail.com>
2020-12-24 20:48:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# What is this?
+This is a npm package that gets random quots!
+
+# installation
+`npm i []`
+
+# How to use :
+```
+const { randomQuote, randomTen } = require('Quote-api')
+
+console.log(randomQuote)
+```
+## Options
+ quote : Get the quote.
+ by : Get who wrote that quote.
+ id : Get the number of the quote. 
---
+++ index.js
@@ -0,0 +1,25 @@
+const quotes = require('./quote.json')
+
+const randomQuote = () => {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+const randomN = (quoteArray, n) => {
+  const limit = quoteArray.length < n ? quoteArray.length : n;
+  const randomIndicesSet = new Set();
+
+  while (randomIndicesSet.size < limit) {
+    const random = Math.floor(Math.random() * quoteArray.length);
+    if (!randomIndicesSet.has(random)) {
+      randomIndicesSet.add(random);
+    }
+  }
+
+  return Array.from(randomIndicesSet).map(random => {
+    return quoteArray[random];
+  });
+};
+
+const randomTen = () => randomN(quotes, 10);
+
+module.exports = { quotes, randomQuote, randomTen };
