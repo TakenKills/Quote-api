4bb95c60ccf009b40117623af0a6c4106ac0bbd2
iColtz <max.hurst@hotmail.com>
2021-01-05 11:22:19 +0000

    added types

--- src/QuoteAPI/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    randomQuote: require('./quoteAPI').randomQuote,
-    randomN: require('./quoteAPI').randomN,
-    randomTen: require('./quoteAPI').randomTen,
-    getbyauthor: require('./quoteAPI').getbyauthor,
-  }
---
+++ src/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  randomQuote: require('./QuoteAPI/quoteAPI').randomQuote,
+  randomN: require('./QuoteAPI/quoteAPI').randomN,
+  randomTen: require('./QuoteAPI/quoteAPI').randomTen,
+  getbyauthor: require('./QuoteAPI/quoteAPI').getbyauthor,
+}
